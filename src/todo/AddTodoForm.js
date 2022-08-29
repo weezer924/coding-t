054cd0a2dd700ff568308b981aaac609b238397c
Jakushin Cho <weezer970@gmail.com>
2022-08-29 18:54:03 +0900

    ToDoを入力するフォームを作成

---
+++ src/todo/AddTodoForm.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+function AddTodoForm (props) {
+  const [text, setTodoText] = useState('')
+
+  const doChange = (e) => {
+    setTodoText(e.target.value)
+  }
+
+  const doAction = (e) => {
+    props.onAddTodo(text);
+    setTodoText('')
+  }
+
+  return (
+    <form onSubmit={doAction} action="">
+      <div className="form-group row">
+        <input type="text" className="form-control-sm col" onChange={doChange} value={text} required />
+        <input type="submit" value="追加" className="btn btn-primary btn-sm col-2" />
+      </div>
+    </form>
+  )
+}
+
+export default AddTodoForm
